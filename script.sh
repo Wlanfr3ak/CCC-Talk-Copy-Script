826e76886a9ac0044e11c409a9c157031edc3277
Fabian Horst <6292882+Wlanfr3ak@users.noreply.github.com>
2018-12-28 14:43:46 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 cd /mnt/harddisk3/35c3/
+
+#Master MP4 1080p with all audio tracks (including translations) and slides as secondary video track (use VLC!) – about 120 GiB:
 curl https://media.ccc.de/c/35c3/podcast/mp4-master.xml | awk -F '"' '/url="/{print $2}' | wget -c -i -
+
+#MP4 1080p with original audio track (without translations) – about 62 GiB:
 curl https://media.ccc.de/c/35c3/podcast/mp4.xml | awk -F '"' '/url="/{print $2}' | wget -c -i -
+
+#WebM 1080p with all audio tracks (including translations) and slides as secondary video track (use VLC!) – about 100 GiB:
 curl https://media.ccc.de/c/35c3/podcast/webm.xml | awk -F '"' '/url="/{print $2}' | wget -c -i -
+
+#MP4 576p with all audio tracks (including translations) and slides as secondary video track – about 11 GiB:
 curl https://media.ccc.de/c/35c3/podcast/mp4-lq.xml | awk -F '"' '/url="/{print $2}' | wget -c -i -
+
+#WebM 576p with all audio tracks (including translations) and slides as secondary video track – about 80? GiB:
 curl https://media.ccc.de/c/35c3/podcast/webm-lq.xml | awk -F '"' '/url="/{print $2}' | wget -c -i -
+
+# https://events.ccc.de/congress/2018/wiki/index.php/Static:Streams#Downloads 
